819ba967c42cb94f9f4823d3f017ca12167780cf
Kelly Lee <amorfati38@gmail.com>
2022-07-21 23:31:33 -0600

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {   
+            var book = new Book("Student's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(52.5);
+            book.AddGrade(35.6);
+
+            var stats = book.GetStatistics();
+            
+            Console.WriteLine($"The highest grade is {stats.Low}.");
+            Console.WriteLine($"The lowest grade is {stats.High}.");
+
+            Console.WriteLine($"The average grade is {stats.Average:N1}.");
+        }
+    }
+}
+
